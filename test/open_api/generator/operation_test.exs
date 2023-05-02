c8be7f6cbeb45870b472f8453d1e6afa4cca040c
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2023-05-01 23:15:40 -0400

    Fix naming-related tests due to module rename

---
+++ test/open_api/generator/operation_test.exs
@@ -0,0 +1,92 @@
+defmodule OpenAPI.Generator.OperationTest do
+  use ExUnit.Case, async: true
+
+  alias OpenAPI.Generator.Operation
+  alias OpenAPI.Spec.Path
+
+  describe "names/1" do
+    test "names a tagless operation" do
+      assert [{[], "example_op"}] =
+               Operation.names(%Path.Operation{operation_id: "example_op", tags: []})
+
+      assert [{[], "example_op"}] =
+               Operation.names(%Path.Operation{operation_id: "exampleOp", tags: []})
+
+      assert [{["ModName"], "example_op"}] =
+               Operation.names(%Path.Operation{operation_id: "mod_name/example_op", tags: []})
+
+      assert [{["ModName"], "example_op"}] =
+               Operation.names(%Path.Operation{operation_id: "modName/exampleOp", tags: []})
+
+      assert [{["Other", "ModName"], "example_op"}] =
+               Operation.names(%Path.Operation{
+                 operation_id: "other/mod_name/example_op",
+                 tags: []
+               })
+
+      assert [{["Other", "ModName"], "example_op"}] =
+               Operation.names(%Path.Operation{operation_id: "other/modName/exampleOp", tags: []})
+
+      assert [{["ModName"], "example_op"}] =
+               Operation.names(%Path.Operation{operation_id: "mod-name/example-op", tags: []})
+    end
+
+    test "names a tagged operation" do
+      assert [{["ModName"], "example_op"}] =
+               Operation.names(%Path.Operation{operation_id: "example_op", tags: ["mod_name"]})
+
+      assert [{["ModName"], "example_op"}] =
+               Operation.names(%Path.Operation{operation_id: "exampleOp", tags: ["modName"]})
+
+      assert [{["ModName"], "example_op"}] =
+               Operation.names(%Path.Operation{
+                 operation_id: "mod_name/example_op",
+                 tags: ["mod_name"]
+               })
+
+      assert [{["ModName"], "example_op"}] =
+               Operation.names(%Path.Operation{
+                 operation_id: "modName/exampleOp",
+                 tags: ["mod_name"]
+               })
+
+      assert [{["ModName"], "example_op"}] =
+               Operation.names(%Path.Operation{
+                 operation_id: "mod_name/example_op",
+                 tags: ["modName"]
+               })
+
+      assert [{["ModName"], "example_op"}] =
+               Operation.names(%Path.Operation{
+                 operation_id: "modName/exampleOp",
+                 tags: ["modName"]
+               })
+
+      assert [{["Other"], "mod_name_example_op"}] =
+               Operation.names(%Path.Operation{
+                 operation_id: "other/mod_name/example_op",
+                 tags: ["other"]
+               })
+
+      assert [{["Other"], "mod_name_example_op"}] =
+               Operation.names(%Path.Operation{
+                 operation_id: "other/modName/exampleOp",
+                 tags: ["other"]
+               })
+
+      assert [{["ModName"], "example_op"}] =
+               Operation.names(%Path.Operation{operation_id: "example-op", tags: ["mod-name"]})
+    end
+
+    test "names an operation with multiple tags" do
+      assert [
+               {["ModName"], "example_op"},
+               {["Other"], "mod_name_example_op"}
+             ] =
+               Operation.names(%Path.Operation{
+                 operation_id: "mod_name/example_op",
+                 tags: ["mod_name", "other"]
+               })
+    end
+  end
+end
