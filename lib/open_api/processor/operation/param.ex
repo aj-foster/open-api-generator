a75bfc4e6d9f9adbed00492fa1257a7b54dca22e
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2023-08-05 00:14:23 -0400

    Add param processor

--- lib/open_api/processor/operation/param.ex
+++ lib/open_api/processor/operation/param.ex
@@ -1,8 +1,44 @@
 defmodule OpenAPI.Processor.Operation.Param do
+  @doc false
+  alias OpenAPI.Processor.Type
+  alias OpenAPI.Spec.Path.Parameter
+  alias OpenAPI.Spec.Schema
+
+  @type location :: :cookie | :header | :path | :query
+
+  @type t :: %__MODULE__{
+          description: String.t() | nil,
+          location: location,
+          name: String.t(),
+          value_type: Type.t()
+        }
+
   defstruct [
     :description,
     :name,
-    :param_type,
+    :location,
     :value_type
   ]
+
+  @spec from_spec(Parameter.t()) :: t
+  def from_spec(%Parameter{} = param) do
+    %Parameter{description: description, name: name} = param
+
+    %__MODULE__{
+      description: description,
+      name: name,
+      location: location(param),
+      value_type: value_type(param)
+    }
+  end
+
+  @spec location(Parameter.t()) :: location
+  defp location(%Parameter{in: "cookie"}), do: :cookie
+  defp location(%Parameter{in: "header"}), do: :header
+  defp location(%Parameter{in: "path"}), do: :path
+  defp location(%Parameter{in: "query"}), do: :query
+
+  @spec value_type(Parameter.t()) :: Type.t()
+  defp value_type(%Parameter{schema: %Schema{} = schema}), do: Type.from_schema(schema)
+  defp value_type(_param), do: {:string, :generic}
 end
