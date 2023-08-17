17d56dc1b3e1726a77eeb87847059e2b32d60bb6
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2023-08-16 23:16:58 -0400

    Fix tests

--- test/open_api/processor/ignore_test.exs
+++ test/open_api/processor/ignore_test.exs
@@ -13,41 +13,39 @@ defmodule OpenAPI.Processor.IgnoreTest do
     %{operation: operation, schema: schema, state: state}
   end
 
-  describe "ignored?/2" do
-    # Operations
-
+  describe "ignore_operation?/2" do
     test "does not ignore operation by default", %{operation: operation, state: state} do
-      refute Ignore.ignored?(state, operation)
+      refute Ignore.ignore_operation?(state, operation)
     end
 
     test "ignores operation by ID", %{operation: operation, state: state} do
       operation = %OpenAPI.Spec.Path.Operation{operation | operation_id: "IgnoredOperation"}
       Application.put_env(:oapi_generator, @profile, ignore: ["IgnoredOperation"])
-      assert Ignore.ignored?(state, operation)
+      assert Ignore.ignore_operation?(state, operation)
     end
 
     test "ignores operation by ID pattern", %{operation: operation, state: state} do
       operation = %OpenAPI.Spec.Path.Operation{operation | operation_id: "IgnoredOperation"}
       Application.put_env(:oapi_generator, @profile, ignore: [~r/^Ignored/])
-      assert Ignore.ignored?(state, operation)
+      assert Ignore.ignore_operation?(state, operation)
     end
 
     test "ignores operation by path", %{operation: operation, state: state} do
       operation = %OpenAPI.Spec.Path.Operation{operation | "$oag_path": "/ignore/this"}
       Application.put_env(:oapi_generator, @profile, ignore: ["/ignore/this"])
-      assert Ignore.ignored?(state, operation)
+      assert Ignore.ignore_operation?(state, operation)
     end
 
     test "ignores operation by path pattern", %{operation: operation, state: state} do
       operation = %OpenAPI.Spec.Path.Operation{operation | "$oag_path": "/ignore/this"}
       Application.put_env(:oapi_generator, @profile, ignore: [~r"^/ignore/"])
-      assert Ignore.ignored?(state, operation)
+      assert Ignore.ignore_operation?(state, operation)
     end
+  end
 
-    # Schemas
-
+  describe "ignore_schema?/2" do
     test "does not ignore schema by default", %{schema: schema, state: state} do
-      refute Ignore.ignored?(state, schema)
+      refute Ignore.ignore_schema?(state, schema)
     end
 
     test "ignores schema by base file path", %{schema: schema, state: state} do
@@ -56,14 +54,14 @@ defmodule OpenAPI.Processor.IgnoreTest do
 
       Application.put_env(:oapi_generator, @profile, ignore: ["components/schemas/ignored_schema"])
 
-      assert Ignore.ignored?(state, schema)
+      assert Ignore.ignore_schema?(state, schema)
     end
 
     test "ignores schema by base file path pattern", %{schema: schema, state: state} do
       base_file_path = ["components", "schemas", "ignored_schema"]
       schema = %OpenAPI.Spec.Schema{schema | "$oag_base_file_path": base_file_path}
       Application.put_env(:oapi_generator, @profile, ignore: [~r|^components/schemas/ignored|])
-      assert Ignore.ignored?(state, schema)
+      assert Ignore.ignore_schema?(state, schema)
     end
 
     test "ignores schema by ref path", %{schema: schema, state: state} do
@@ -72,26 +70,26 @@ defmodule OpenAPI.Processor.IgnoreTest do
 
       Application.put_env(:oapi_generator, @profile, ignore: ["components/schemas/ignored_schema"])
 
-      assert Ignore.ignored?(state, schema)
+      assert Ignore.ignore_schema?(state, schema)
     end
 
     test "ignores schema by ref path pattern", %{schema: schema, state: state} do
       ref_path = ["components", "schemas", "ignored_schema"]
       schema = %OpenAPI.Spec.Schema{schema | "$oag_last_ref_path": ref_path}
       Application.put_env(:oapi_generator, @profile, ignore: [~r|^components/schemas/ignored|])
-      assert Ignore.ignored?(state, schema)
+      assert Ignore.ignore_schema?(state, schema)
     end
 
     test "ignores schema by title", %{schema: schema, state: state} do
       schema = %OpenAPI.Spec.Schema{schema | title: "IgnoredSchema"}
       Application.put_env(:oapi_generator, @profile, ignore: ["IgnoredSchema"])
-      assert Ignore.ignored?(state, schema)
+      assert Ignore.ignore_schema?(state, schema)
     end
 
     test "ignores schema by title pattern", %{schema: schema, state: state} do
       schema = %OpenAPI.Spec.Schema{schema | title: "IgnoredSchema"}
       Application.put_env(:oapi_generator, @profile, ignore: [~r/^Ignored/])
-      assert Ignore.ignored?(state, schema)
+      assert Ignore.ignore_schema?(state, schema)
     end
   end
 end
