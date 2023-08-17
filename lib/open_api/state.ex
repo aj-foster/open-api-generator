7c4ed79d32f5b0e14f0e89f6ee3c2605879776ed
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2023-08-17 00:03:56 -0400

    Add renderer stub

--- lib/open_api/state.ex
+++ lib/open_api/state.ex
@@ -4,21 +4,11 @@ defmodule OpenAPI.State do
   alias OpenAPI.Call
   alias OpenAPI.Spec
 
-  @type file :: %{
-          name: String.t(),
-          docstring: String.t(),
-          fields: map,
-          methods: [term],
-          using: [module]
-        }
-
   @typedoc "OpenAPI generator state."
   @type t :: %__MODULE__{
           call: Call.t(),
-          operations: [map],
-          operation_files: %{optional(module) => file},
-          schemas: %{optional(String.t()) => map},
-          schema_files: %{optional(module) => file},
+          operations: [OpenAPI.Processor.Operation.t()],
+          schemas: %{reference => OpenAPI.Processor.Schema.t()},
           spec: Spec.t() | nil
         }
 
@@ -30,9 +20,7 @@ defmodule OpenAPI.State do
     :config,
     :files,
     :operations,
-    :operation_files,
     :schemas,
-    :schema_files,
     :spec
   ]
 
@@ -43,9 +31,7 @@ defmodule OpenAPI.State do
       config: OpenAPI.Config.new(Application.get_env(:oapi_generator, call.profile)),
       files: %{},
       operations: [],
-      operation_files: %{},
       schemas: %{},
-      schema_files: %{},
       spec: nil
     }
   end
