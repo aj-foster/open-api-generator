9d0a6836bdea45ad2ae05340f3cb555a5f9e8427
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2023-08-16 23:25:23 -0400

    Document and test grouping schemas

--- test/open_api/processor/naming_test.exs
+++ test/open_api/processor/naming_test.exs
@@ -1,4 +1,30 @@
 defmodule OpenAPI.Processor.NamingTest do
   use ExUnit.Case, async: true
   doctest OpenAPI.Processor.Naming, import: true
+
+  alias OpenAPI.Processor.Naming
+
+  @profile __MODULE__
+
+  setup do
+    %{state: %OpenAPI.Processor.State{profile: @profile}}
+  end
+
+  describe "group_schema/2" do
+    test "does nothing by default", %{state: state} do
+      Application.put_env(:oapi_generator, @profile, naming: [group: []])
+      assert Naming.group_schema({"TestSchema", "t"}, state) == {"TestSchema", "t"}
+    end
+
+    test "groups schema based on prefixes", %{state: state} do
+      Application.put_env(:oapi_generator, @profile, naming: [group: [Test]])
+      assert Naming.group_schema({"TestSchema", "t"}, state) == {"Test.Schema", "t"}
+      assert Naming.group_schema({"MyTest", "t"}, state) == {"MyTest", "t"}
+    end
+
+    test "groups multiple namespaces in order", %{state: state} do
+      Application.put_env(:oapi_generator, @profile, naming: [group: [My, My.Test]])
+      assert Naming.group_schema({"MyTestSchema", "t"}, state) == {"My.Test.Schema", "t"}
+    end
+  end
 end
